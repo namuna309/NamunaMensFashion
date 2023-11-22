b1bde7af19fdf45f4690225f7a3678ee02bf2efa
namuna309 <nyah309@gmail.com>
2023-11-22 11:07:00 +0900

    2023-11-22 oocss 작업 및 그에 따른 js, html 문서 수정

--- index.js
+++ index.js
@@ -114,22 +114,23 @@ let loopInterval = setInterval(() => {
 
 
 // 착장 사진 캐러셀
-var onModel_w = $('.onModel').width();
+var onModel_w = $('.onModel .item').width();
 var onModel_curPic = 0;
-var onModel_maxPicnum = $('.onModel-container img').length;
+var onModel_maxPicnum = $('.onModel .item img').length;
 
 
-setInterval(() => {
-  onModel_curPic = infiniteCarousel('.onModel-container', onModel_curPic, onModel_maxPicnum, onModel_w);
+let onModel_carousel = setInterval(() => {
+  onModel_curPic = infiniteCarousel('.onModel .container', onModel_curPic, onModel_maxPicnum, onModel_w);
 }, 2000);
 
 // 옷 사진 캐러셀
-var cloth_w = $('.cloth-box').width();
-cloth_w += parseInt($('.cloth-box').css('margin-right')) * 2;
+var cloth_w = $('.cloth .item').width();
+cloth_w += parseInt($('.cloth .item').css('margin-right')) * 2;
 var cloth_curPic = 0;
-var cloth_maxPicnum = $('.cloth-container img').length - 2;
-setInterval(() => {
-  cloth_curPic = infiniteCarousel('.cloth-container', cloth_curPic, cloth_maxPicnum, cloth_w)
+var cloth_maxPicnum = $('.cloth .item img').length - 2;
+
+let cloth_carousel = setInterval(() => {
+  cloth_curPic = infiniteCarousel('.cloth .container', cloth_curPic, cloth_maxPicnum, cloth_w)
 
 }, 2000);
 
@@ -139,16 +140,13 @@ function infiniteCarousel(box, pic_num, max_pics, box_width) {
   pic_num++;
   if (pic_num >= max_pics) {
     pic_num = 0;
-    $(box).removeClass('trans_smth');
-    $(box).addClass('trans');
+    $(box).removeClass('trans');
     $(box).css('transform', `translateX(0px)`);
-
+    pic_num++;
     setTimeout(() => {
-      pic_num = 1;
-      $(box).removeClass('trans');
-      $(box).addClass('trans_smth');
+      $(box).addClass('trans');
       $(box).css('transform', `translateX(${(-1) * pic_num * box_width}px)`);
-    }, 0)
+    }, 0);
 
   }
   else {
@@ -183,3 +181,4 @@ function logoBlack() {
   document.querySelector('.background-font-color').style = 'background-color: white;color:black;';
   document.querySelector('.logo-img').style = 'background: url(/images/icon/logo_b.png);background-size: 100% 100%;background-position: 0 0;';
 }
+
