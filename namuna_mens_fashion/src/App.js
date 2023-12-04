666933d5adc2a997bdc7a9a34d1100733d6dd7e9
namuna309 <nyah309@gmail.com>
2023-12-04 18:59:38 +0900

    2023-12-04 React 프로젝트 생성 및 nav바 생성

---
+++ namuna_mens_fashion/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import './CSS/header.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className="header_inner background-font-color">
+            <div className="logo">
+                <div className="logo-img"></div>
+            </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
