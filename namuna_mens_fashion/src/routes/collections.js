25fd70bba68f3b47541a32993aa6ece917594d08
namuna309 <nyah309@gmail.com>
2023-12-14 18:29:32 +0900

    2023-12-14 collections 페이지 완성

--- namuna_mens_fashion/src/routes/collections.js
+++ namuna_mens_fashion/src/routes/collections.js
@@ -1,52 +1,61 @@
-//CSS
+// CSS
 import './../css/collections.css';
 
 // Image
-import FW2023 from './../img/collections/2023_FW/namuna_men_fall_winter_runway_look_2023_1.png';
+import banner from './../img/banner/collectionsBanner_1.png'
 
-
-// const images = require.context('./../img/collections/2023_FW', true);
-// const imgList = images.keys().map((img) => images(img));
+// Library
+import { useState } from 'react';
 
 const images_all = require.context('./../img/collections', true);
-const allimgList = images_all.keys().map((img) => images_all(img));
+let seasons = new Set();
+
+// 이미지 파일 경로 및 시즌 추출
+const allimgList = images_all.keys().map((img) => {
+    let season = img.split('/')[1];
+    seasons.add(season)
+    return [season, images_all(img)];
+});
+
+// 시즌 별 이미지 소스 분류
+seasons = [...seasons].reverse();
+
+let collections = new Object();
+
+seasons.map((season) => {
+    collections[season] = new Array();
+}) 
+
+allimgList.map((img) => {
+    collections[img[0]].push(img[1]);
+})
 
 function Collections() {
+
+    let [sortedSeason, setSeason] = useState(seasons[0]);
     
     return (
         <div className="collections">
+            <div className='collections-banner'><img src={banner}/></div>
+            <div style={{width: '100%', height: '1px'}}></div>
+            <div className='season-sort'>
+                {
+                    seasons.map((season) => <div><p onClick={() => setSeason(season)}>{season}</p></div>)
+                }
+            </div>
+            <div style={{width: "100%", height: "1px"}}></div>
             <div className='collcetions-containter'>
                 {   
+                    collections[sortedSeason].map((imgSrc, idx) => {
+                            return (
+                                <div className='collections-box'>
+                                                    <div className='collections-img'><img src={imgSrc}/></div>
+                                                    <div className='collections-title'><h6>{sortedSeason} Look {idx + 1}</h6></div>
+                                                </div>
+                            )
+                        })
                     
-                    allimgList.map((imgSrc, idx) => {
-                        return(
-                            <div className='collections-box'>
-                                <div className='collections-img' ><img src={imgSrc} /></div>
-                                <div className='collections-title'><h6>FW2023 Look {idx + 1}</h6></div>
-                            </div>
-                        )
-                    })
                 }
-                {/* <div className='collections-box'>
-                    <div className='collections-img' ><img src={FW2023} /></div>
-                    <div className='collections-title'><h5>FW2023</h5></div>
-                </div>
-                <div className='collections-box'>
-                    <div className='collections-img' ><img src={FW2023} /></div>
-                    <div className='collections-title'><h5>FW2023</h5></div>
-                </div>
-                <div className='collections-box'>
-                    <div className='collections-img' ><img src={FW2023} /></div>
-                    <div className='collections-title'><h5>FW2023</h5></div>
-                </div>
-                <div className='collections-box'>
-                    <div className='collections-img' ><img src={FW2023} /></div>
-                    <div className='collections-title'><h5>FW2023</h5></div>
-                </div>
-                <div className='collections-box'>
-                    <div className='collections-img' ><img src={FW2023} /></div>
-                    <div className='collections-title'><h5>FW2023</h5></div>
-                </div> */}
             </div>
         </div>
     )
